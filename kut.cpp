a575af695a32e357ea7e67ed4287528d7fd24170
Swaraj-Singh-30 <swarajsingh3020@gmail.com>
2026-03-20 10:49:20 +0530

    Implement raw mode for terminal input handling

--- kut.cpp
+++ kut.cpp
@@ -1,7 +1,54 @@
+/*** includes ***/
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
+#include <stdio.h>
+using namespace std;
 
+/*** data ***/
+struct termios orig_termios;
+
+/*** terminal ***/
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // Disable Ctrl-s and Ctrl-q for flow control.
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); // Disable echo, canonical mode, and signals so that Ctrl-c does not kill the program and Ctrl-z does not suspend it and Ctrl-v does not insert the next character verbatim.
+  
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+/*** init ***/
 int main() {
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  enableRawMode();
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+  }
   return 0;
 }
